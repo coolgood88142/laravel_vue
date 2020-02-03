e846509fc2b928170e81f40bf31255574d11237d
Kai <coolgood88142@gmail.com>
2020-02-04 03:59:15 +0800

    vue-style新增個人資料表單使用vue(未完成:暫時完成頁面之後再寫component)

--- resources/js/style.js
+++ resources/js/style.js
@@ -1,4 +1,4 @@
-import BootstrapVue from 'bootstrap-vue'
+// import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue);
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Vue.use(BootstrapVue);
