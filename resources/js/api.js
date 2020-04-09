02b77c2cbe5bf568d02a32b70894875ac5f4ca38
Kai <coolgood88142@gmail.com>
2020-04-10 03:12:52 +0800

    google表單(問卷)填寫完新增資料到試算表上面，在用google app script秀在頁面上(完成)，問卷連結:https://forms.gle/QzShsKDUykTs1ktL6

--- resources/js/api.js
+++ resources/js/api.js
@@ -8,7 +8,7 @@ let app = new Vue({
         users: []
     },
     mounted() {
-        axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=ZeY4yML5iYmTjGM1LaL59Zr6_FrRJCahfDnX7hooypOYUFu3h4UPY8YpW0pnMrlV2V-BQeMJ26PecXGdpRkAxRwqRqAJukjMm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCXgUTRdgqXj3Aq3ZTlpz-TBJKUm1HAR0A93zL_jSiszuca-6WkGG1kr6HoIsCLXONveWPraiRQB&lib=MxgXVhpOa8KXE62faMUKC2eGkeYm2IhVF')
+        axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=gTMpSUFN0h49HLC_YD9XPQ9NIIDCB0enU5hjbScjMUgCKNt5SGbST0ARVjvjzolI4ysqYt-c0a4W2hoRltslCPvMgwsyepKfm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCXgUTRdgqXj3Aq3ZTlpz-TBJKUm1HAR0A93zL_jSiszuca-6WkGG1kr6HoIsCLXONveWPraiRQB&lib=MxgXVhpOa8KXE62faMUKC2eGkeYm2IhVF')
         .then(
             response => (
             this.users = response.data
