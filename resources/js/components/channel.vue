8957668f3e45f5bb8f716422a3f318ad21f0de0b
Kai <coolgood88142@gmail.com>
2020-04-21 03:03:18 +0800

    頻道選單功能要重構(未完成:剩下master在選擇時要組資料，程式有拋錯還在看)

--- resources/js/components/channel.vue
+++ resources/js/components/channel.vue
@@ -7,19 +7,15 @@
             <table id="example" class="table table-striped table-bordered" style="width:100%">
                 <tr v-for="(el,ListIndex) in channelList" :key="ListIndex">
                     <th v-for="(selectData,index) in el" :key="index">
-                        <div v-if="selectData.isSelect">
-                            <select v-model="modelData[index][ListIndex]" :name="selectData.elName" @change="getSubData(selectData.isMaster, ListIndex)">
-                                <option value="" disabled selected>--請選擇--</option>
-                                <option v-for="(data, index) in selectData.select" :key="index" :value="data.value">{{ data.text }}</option>
-                            </select>
-                        </div>
-                        <div v-else>
-                            <input type="button" :class="btnDelete" value="刪除" v-on:click="delChannel(ListIndex)">
-                        </div>
+                        <select v-model="modelData[index][ListIndex]" :name="selectData.elName" @change="getSubData(selectData.isMaster, ListIndex)">
+                            <option value="" disabled selected>--請選擇--</option>
+                            <option v-for="(data, index) in selectData.select" :key="index" :value="data.value">{{ data.text }}</option>
+                        </select>
+                    </th>
+                    <th>
+                        <input type="button" :class="btnDelete" value="刪除" v-on:click="delChannel(ListIndex)">
                     </th>
                 </tr>
-                <!-- 這個不需要直接在新增與刪除去計算目前有多少組資料就可以了-->
-                <!-- <input type="hidden" v-model="nowChannelCount" name="channelIndex"> -->
             </table>
         </div>
     </div>
@@ -36,64 +32,53 @@ export default {
         },
         subSelected:{
             type:Array
-        },
-        masterModel:{
-            type:Array
-        },
-        subModel:{
-            type:Array
-        },
-        channelIndex:{
-            type:Number
         }
     },
     data:function(){
         return {
-            nowChannelCount: this.channelIndex,
             //不應該這寫，當function或參數寫壞了，導致回傳資料錯誤變成null的話，畫面全壞，預設值的定義是一定是正常的資料型態
             channelList: [],
             btnAdd:'btn btn-primary',
             btnDelete:'btn btn-secondary',
             modelData: [this.masterSelected,this.subSelected],
-            modelName: [this.masterModel,this.subModel],
             buttonData: {isSelect : false},
-            isDafult: true
+            isDafult: true,
+            channelIindex:0
         }
     },
     computed: {
-        channelList(){
-            return this.getChannelData(this.masterSelected, this.subSelected)
-        }
+        // channelList(){
+        //     return this.getChannelData()
+        // },
+        // channelIindex(){
+        //     return this.channelList.length - 1
+        // }
     },
     mounted() {
-        // this.setSubData(this.isDafult, this.masterSelected)
+        this.channelList = this.getChannelData()
     },
     methods: {
-        setSubData: function(isDafult, masterSelected) {
-            if(isDafult){
-                for(let i = 0; i < masterSelected.length; i++){
-                    this.getSubData(true,i)
-                }
-            }
-        },
-        getChannelData :function(masterSelected, subSelected){
+        getChannelData :function(){
+            let master_selected = this.modelData[0]
+            let sub_selected = this.modelData[1]
             let data = []
-            if(masterSelected.length > 0 && subSelected.length > 0 ){
-                for(let i = 0; i < masterSelected.length; i++){
+            if(master_selected.length > 0 && sub_selected.length > 0 ){
+                for(let i = 0; i < master_selected.length; i++){
                     //因為要選擇master選單後，才可以產生sub選單，所以要拆開寫
                     let master = this.getSelectData(this.channelData, i)
                     let sub = this.getSelectData(null, i)
-                    data.push([master, sub, {isSelect : false}])
-                    this.nowChannelCount = i+1
+                    data.push([master, sub])
                 }
             }
+
+            this.channelIindex = data.length - 1
 
             return data
         },
         getChannelList: function(index){
-            return [this.getSelectData(this.channelData,index), this.getSelectData(null, index), {isSelect : false}]
+            return [this.getSelectData(this.channelData,index), this.getSelectData(null, index)]
         },
-        getSelectData: function(channelData, index){
+        getSelectData: function(channelData, count){
             let selectData = []
             let isMaster = false
             let elName = 'sub[]'
@@ -104,12 +89,22 @@ export default {
                 data.forEach(function(el,index){
                     selectData.push({text:el, value:index})
                 });
-                this.masterModel[index] = 'master' + index
             }else{
-                this.subModel[index] = 'sub' + index
+                let channel = this.channelData
+                let obj = Object.keys(channel)
+                let master = this.modelData[0]
+                obj.forEach(function(el,index){
+                    if(master[count] == index){
+                        let sub_data = channel[el]
+                        let sub = Object.keys(sub_data)
+                        sub.forEach(function(el,index){
+                            selectData.push({text:el, value:sub_data[el]})
+                        })
+                    }
+                });
             }
 
-            return {isSelect : true, isMaster: isMaster, select: selectData, elName: elName}
+            return {isMaster: isMaster, select: selectData, elName: elName}
         },
         getSubData: function(isMaster, index){
             if(isMaster){
@@ -130,39 +125,19 @@ export default {
         },
         addChannel: function(){
             this.isDafult = false
-            this.nowChannelCount = this.nowChannelCount+1
-            this.channelList.push(this.getChannelList(this.nowChannelCount))
-            this.modelData[0][this.nowChannelCount] = ''
-            this.modelData[1][this.nowChannelCount] = ''
+            let index = this.channelIindex + 1
+            this.modelData[0][index] = ''
+            this.modelData[1][index] = ''
+            this.channelList = this.getChannelData()
         },
         delChannel: function(index){
             this.isDafult = false
             let master_value = document.getElementsByName('master[]')[index].value
             let sub_value = document.getElementsByName('sub[]')[index].value
-            this.channelList.splice(index,1)
             this.modelData[0].splice(index,1)
             this.modelData[1].splice(index,1)
-            this.masterModel.splice(index,1)
-            this.subModel.splice(index,1)
-            // this.modelName[0].splice(index,1)
-            // this.modelName[1].splice(index,1)
-            this.updateModelData()
-            this.nowChannelCount = this.nowChannelCount-1
         },
-        updateModelData: function(){
-            let master = []
-            let sub = []
-            this.masterModel.forEach(function(el,index){
-                master.push('master' + index)
-            })
 
-            this.subModel.forEach(function(el,index){
-                sub.push('sub' + index)
-            })
-
-            this.modelName[0] = master
-            this.modelName[1] = sub
-        }
     }
 }
 </script>
