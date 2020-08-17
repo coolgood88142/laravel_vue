23060f84b60c79ce837d2eb58ded75d26d79b258
Kai <coolgood88142@gmail.com>
2020-08-18 04:16:05 +0800

    Vue優化信用卡編輯功能，功能已完成

---
+++ resources/js/components/repeatCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+                <div class="modal-header">
+                    <slot name="header">
+                        <h3>資料有重複</h3>
+                    </slot>
+                </div>
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <div class="form-check form-check-inline">
+                            <input type="button" class="btn btn-primary" id="save" name="save" value="確認" @click="$emit('close')">
+                        </div>
+                    </slot>
+                </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+</template>
+
+<script>
+export default {
+
+}
+</script>>
